cca6bc8fbb8a44f8f03f70728a05756fcc89d287
lokithe5th <lourenslinde@gmail.com>
2024-06-07 15:52:15 +0200

    Feat: add trace for invest fail-case

--- test/invariant/CryticToFoundry.sol
+++ test/invariant/CryticToFoundry.sol
@@ -9,14 +9,23 @@ import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         vm.createSelectFork("mainnet");
+        uint256 cacheTimestamp = block.timestamp;
         setup();
+        vm.warp(cacheTimestamp);
     }
 
+    // Simple test to investigate stale oracle
     function test_oracleLiveness() public {
         console2.log("Time: ", block.timestamp);
         vm.startPrank(compoundTimelock);
         trustSetup.invest();
         trustSetup.commenceDivestment(100 ether);
         trustSetup.completeDivestment();
+    }
+
+    // Run with: `forge test --match-test test_investFail -vvvv`
+    function test_investFail() public {
+        balancer_swap(0,10457393105336675900);
+        trustSetup_invest();
     }
 }
