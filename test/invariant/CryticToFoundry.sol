35357693a0e5edbc57146baf7641bfb2e708dadb
lokithe5th <lourenslinde@gmail.com>
2024-06-07 16:01:58 +0200

    Fix: log the de,lta

--- test/invariant/CryticToFoundry.sol
+++ test/invariant/CryticToFoundry.sol
@@ -6,6 +6,10 @@ import {Test, console2} from "forge-std/Test.sol";
 import {TargetFunctions} from "./targets/TargetFunctions.sol";
 import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 
+interface IActualSupply {
+  function getActualSupply() external returns (uint256);
+}
+
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         vm.createSelectFork("mainnet");
@@ -26,6 +30,12 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     // Run with: `forge test --match-test test_investFail -vvvv`
     function test_investFail() public {
         balancer_swap(0,10457393105336675900);
+
+        console2.log("Total Supply: ", pool.totalSupply());
+        console2.log("Actual Supply: ", IActualSupply(address(pool)).getActualSupply());
+        console2.log("Delta: ", IActualSupply(address(pool)).getActualSupply() - pool.totalSupply());
+        console2.log("");
+
         trustSetup_invest();
     }
 }
